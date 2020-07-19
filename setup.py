e871f949685292414d00671790e2d0ae90352b31
cheginit <taher.chegini@gmail.com>
2020-07-19 03:53:05 -0500

    add license

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(Path(__file__).with_name("README.rst")) as f:
     long_description = f.read()
 
 setup(
-    version="0.1.2",
+    version="0.1.3",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     install_requires=parse_requirements("requirements.txt"),
