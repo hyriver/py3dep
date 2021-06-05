1ec2646627fe63977249f901ed621c9a1f163cce
cheginit <cheginit@gmail.com>
2021-06-04 21:04:08 -0500

    ENH: Replace repeated strings with a variable and use global vars instead of fixtures [skip ci]

--- tests/test_py3dep.py
+++ tests/test_py3dep.py
@@ -15,21 +15,18 @@ import py3dep
 
 DEF_CRS = "epsg:4326"
 ALT_CRS = "epsg:3857"
-
-
-@pytest.fixture
-def geometry():
-    return Polygon(
-        [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
-    )
+GEOM = Polygon(
+    [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
+)
+LYR = "Slope Degrees"
 
 
 @pytest.mark.flaky(max_runs=3)
-def test_getmap(geometry):
-    lyr = ["DEM", "Slope Degrees"]
-    ds = py3dep.get_map(lyr, geometry.bounds, 1e3, geo_crs=DEF_CRS, crs=ALT_CRS)
-    dem_10 = py3dep.get_map(lyr[0], geometry, 10, geo_crs=DEF_CRS, crs=ALT_CRS)
-    dem_1e3 = py3dep.get_map(lyr[0], geometry, 1e3, geo_crs=DEF_CRS, crs=ALT_CRS)
+def test_getmap():
+    layers = ["DEM", LYR]
+    ds = py3dep.get_map(layers, GEOM.bounds, 1e3, geo_crs=DEF_CRS, crs=ALT_CRS)
+    dem_10 = py3dep.get_map(layers[0], GEOM, 10, geo_crs=DEF_CRS, crs=ALT_CRS)
+    dem_1e3 = py3dep.get_map(layers[0], GEOM, 1e3, geo_crs=DEF_CRS, crs=ALT_CRS)
     assert (
         sorted(ds.keys()) == ["elevation", "slope_degrees"]
         and abs(dem_10.mean().item() - dem_1e3.mean().item()) < 7e-2
@@ -42,16 +39,16 @@ def test_coords():
 
 
 @pytest.mark.flaky(max_runs=3)
-def test_deg2mpm(geometry):
-    slope = py3dep.get_map("Slope Degrees", geometry, 1e3)
+def test_deg2mpm():
+    slope = py3dep.get_map(LYR, GEOM, 1e3)
     slope = py3dep.deg2mpm(slope)
     assert abs(slope.mean().item() - 0.05) < 1e-3
 
 
-def test_grid(geometry):
+def test_grid():
     geo_crs = DEF_CRS
     crs = "+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"
-    geom = MatchCRS(geo_crs, crs).geometry(geometry)
+    geom = MatchCRS(geo_crs, crs).geometry(GEOM)
     xmin, ymin, xmax, ymax = geom.bounds
     res = 1e3
     gx = np.arange(xmin, xmax, res)
@@ -60,13 +57,12 @@ def test_grid(geometry):
     assert abs(elev.mean().item() - 295.763) < 1e-3
 
 
-def test_cli_map(script_runner, geometry):
-    gdf = gpd.GeoDataFrame({"id": "geo_test", "res": 1e3}, geometry=[geometry], index=[0])
-    gdf.to_file("nat_geo.gpkg")
-    ret = script_runner.run(
-        "py3dep", "nat_geo.gpkg", "geometry", DEF_CRS, "-l", "Slope Degrees", "-s", "geo_map"
-    )
-    shutil.rmtree("nat_geo.gpkg")
+def test_cli_map(script_runner):
+    gdf = gpd.GeoDataFrame({"id": "geo_test", "res": 1e3}, geometry=[GEOM], index=[0])
+    geo_gpkg = "nat_geo.gpkg"
+    gdf.to_file(geo_gpkg)
+    ret = script_runner.run("py3dep", geo_gpkg, "geometry", DEF_CRS, "-l", LYR, "-s", "geo_map")
+    shutil.rmtree(geo_gpkg)
     shutil.rmtree("geo_map")
     assert ret.success
     assert "Retrieved topography data for 1 item(s)." in ret.stdout
@@ -75,9 +71,10 @@ def test_cli_map(script_runner, geometry):
 
 def test_cli_coords(script_runner):
     df = pd.DataFrame([(-7766049.664788851, 5691929.739021257)] * 3, columns=["x", "y"])
-    df.to_csv("coords.csv")
-    ret = script_runner.run("py3dep", "coords.csv", "coords", ALT_CRS, "-s", "geo_coords")
-    Path("coords.csv").unlink()
+    coord_csv = "coords.csv"
+    df.to_csv(coord_csv)
+    ret = script_runner.run("py3dep", coord_csv, "coords", ALT_CRS, "-s", "geo_coords")
+    Path(coord_csv).unlink()
     shutil.rmtree("geo_coords")
     assert ret.success
     assert "Retrieved elevation data for 3 item(s)." in ret.stdout
