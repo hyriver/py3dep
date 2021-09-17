a39b77e6f35a7fe1139442953a3d94c74cef30ab
cheginit <cheginit@gmail.com>
2021-09-16 19:41:08 -0500

    ENH: Add namespace for doctest

---
+++ conftest.py
@@ -0,0 +1,11 @@
+"""Configuration for pytest."""
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def add_standard_imports(doctest_namespace):
+    """Add py3dep namespace for doctest."""
+    import py3dep
+
+    doctest_namespace["py3dep"] = py3dep
