758c02da29d582350cda03ff3c5c9dcde828e0c7
cheginit <cheginit@gmail.com>
2021-10-01 11:23:31 -0500

    ENH: Add a new test [skip ci]

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,3 +1,13 @@
+import numpy as np
 import pytest
+import xarray as xr
 
-from py3dep import MissingColumns, MissingCRS, MissingDependency
+import py3dep
+from py3dep import MissingAttribute, MissingColumns, MissingCRS, MissingDependency
+
+
+def test_missing_nodata():
+    with pytest.raises(MissingAttribute) as ex:
+        dem = xr.DataArray(np.random.randn(2, 3), dims=("x", "y"), coords={"x": [10, 20]})
+        _ = py3dep.fill_depressions(dem)
+        assert "nodatavals" in str(ex.value)
