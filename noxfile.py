4a641362edcbcb16956f38abb4cfda3e954fda92
cheginit <cheginit@gmail.com>
2021-10-05 13:56:59 -0500

    ENH: Add the tree flag to pip install [skip ci]

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ def tests(session):
     session.install(".[test,dem]")
     hr_deps = ["pygeoogc", "pygeoutils"]
     for p in hr_deps:
-        session.install(f"git+https://github.com/cheginit/{p}.git")
+        session.install(f"git+https://github.com/cheginit/{p}.git", "--use-feature=in-tree-build")
     session.run("pytest")
     session.run("coverage", "report")
     session.run("coverage", "html")
