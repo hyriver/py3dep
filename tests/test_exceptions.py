dd44a82cb5f073bd76d8bcb9f8d3a6ffdfcf23a2
cheginit <cheginit@gmail.com>
2022-01-14 19:42:42 -0600

    ENH: Remove unused imports and add typeguard skip for exceptions [skip ci]

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3,14 +3,21 @@ from shapely.geometry import Polygon
 
 import py3dep
 from py3dep import InvalidInputType, InvalidInputValue
-from py3dep.cli import cli
 
 DEF_CRS = "epsg:4326"
 GEOM = Polygon(
     [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
 )
 
+try:
+    import typeguard  # noqa: F401
+except ImportError:
+    has_typeguard = False
+else:
+    has_typeguard = True
 
+
+@pytest.mark.skipif(has_typeguard, reason="Broken if Typeguard is enabled")
 def test_wrong_bbox():
     with pytest.raises(InvalidInputType) as ex:
         _ = py3dep.check_3dep_availability((1, 2, 3))
@@ -23,7 +30,7 @@ def test_wrong_layer():
         assert "DEM" in str(ex.value)
 
 
-def test_wrong_bbox():
+def test_wrong_crs():
     with pytest.raises(InvalidInputValue) as ex:
         _ = py3dep.get_map("None", GEOM.bounds, 1e3, DEF_CRS, "ESRI:102003")
         assert "crs" in str(ex.value)
