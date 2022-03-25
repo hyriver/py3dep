437aaee0b658757718bb1363b7faee2e717f5323
cheginit <cheginit@gmail.com>
2022-03-25 00:46:29 -0500

    ENH: Add two new functions for getting elevation profile along a line, elevation_profile, and querying 3dep's data source within a bounding box, query_3dep_souce. [skip ci]

--- py3dep/__init__.py
+++ py3dep/__init__.py
@@ -7,27 +7,35 @@ from .exceptions import (
     MissingDependency,
 )
 from .print_versions import show_versions
-from .py3dep import check_3dep_availability, elevation_bycoords, elevation_bygrid, get_map
+from .py3dep import (
+    check_3dep_availability,
+    elevation_bycoords,
+    elevation_bygrid,
+    elevation_profile,
+    get_map,
+    query_3dep_sources,
+)
 from .utils import deg2mpm, fill_depressions
 
 try:
-    import importlib.metadata as metadata
+    import importlib.metadata
 except ImportError:
-    import importlib_metadata as metadata  # type: ignore[no-redef]
+    import importlib_metadata
 
-try:
-    __version__ = metadata.version("py3dep")
-except Exception:
-    __version__ = "999"
+    __version__ = importlib_metadata.version("py3dep")
+else:
+    __version__ = importlib.metadata.version("py3dep")
 
 __all__ = [
     # Functions
     "fill_depressions",
     "get_map",
     "check_3dep_availability",
+    "query_3dep_sources",
     "deg2mpm",
     "elevation_bycoords",
     "elevation_bygrid",
+    "elevation_profile",
     "show_versions",
     # Exceptions
     "MissingColumns",
--- py3dep/py3dep.py
+++ py3dep/py3dep.py
@@ -2,19 +2,25 @@
 import contextlib
 import itertools
 from dataclasses import dataclass
-from typing import Dict, List, Sequence, Tuple, Union
+from typing import Dict, List, Optional, Sequence, Tuple, Union
 
 import async_retriever as ar
 import cytoolz as tlz
 import geopandas as gpd
+import numpy as np
+import pandas as pd
 import pygeoutils as geoutils
 import pyproj
 import rioxarray  # noqa: F401
+import shapely.ops as ops
 import xarray as xr
-from pygeoogc import WMS, ArcGISRESTful, ServiceError, ServiceURL
+from async_retriever import ServiceError
+from pygeoogc import WMS, ArcGISRESTful, ServiceURL
 from pygeoogc import utils as ogc_utils
+from pygeoutils import GeoBSpline
+from pygeoutils.pygeoutils import Spline
 from rasterio.enums import Resampling
-from shapely.geometry import MultiPolygon, Polygon
+from shapely.geometry import LineString, MultiLineString, MultiPolygon, Polygon
 
 from . import utils
 from .exceptions import InvalidInputType, InvalidInputValue
@@ -35,7 +41,14 @@ LAYERS = [
     "Contour 25",
     "Contour Smoothed 25",
 ]
-__all__ = ["get_map", "elevation_bygrid", "elevation_bycoords", "check_3dep_availability"]
+__all__ = [
+    "get_map",
+    "elevation_bygrid",
+    "elevation_bycoords",
+    "elevation_profile",
+    "check_3dep_availability",
+    "query_3dep_sources",
+]
 
 
 def get_map(
@@ -44,8 +57,6 @@ def get_map(
     resolution: float,
     geo_crs: Union[str, pyproj.CRS] = DEF_CRS,
     crs: Union[str, pyproj.CRS] = DEF_CRS,
-    expire_after: int = EXPIRE,
-    disable_caching: bool = False,
 ) -> Union[xr.Dataset, xr.DataArray]:
     """Access to `3DEP <https://www.usgs.gov/core-science-systems/ngp/3dep>`__ service.
 
@@ -82,10 +93,6 @@ def get_map(
         ``EPSG:4326``. Valid values are ``EPSG:4326``, ``EPSG:3576``, ``EPSG:3571``,
         ``EPSG:3575``, ``EPSG:3857``, ``EPSG:3572``, ``CRS:84``, ``EPSG:3573``,
         and ``EPSG:3574``.
-    expire_after : int, optional
-        Expiration time for response caching in seconds, defaults to -1 (never expire).
-    disable_caching : bool, optional
-        If ``True``, disable caching requests, defaults to False.
 
     Returns
     -------
@@ -108,15 +115,7 @@ def get_map(
         raise InvalidInputValue("crs", valid_crs)
 
     _geometry = geoutils.geo2polygon(geometry, geo_crs, crs)
-    wms = WMS(
-        wms_url,
-        layers=_layers,
-        outformat="image/tiff",
-        crs=crs,
-        expire_after=expire_after,
-        disable_caching=disable_caching,
-        validation=False,
-    )
+    wms = WMS(wms_url, layers=_layers, outformat="image/tiff", crs=crs, validation=False)
     r_dict = wms.getmap_bybox(_geometry.bounds, resolution, box_crs=crs)
     if isinstance(geometry, (Polygon, MultiPolygon)):
         ds = geoutils.gtiff2xarray(r_dict, _geometry, crs)
@@ -132,8 +131,6 @@ def elevation_bygrid(
     crs: Union[str, pyproj.CRS],
     resolution: float,
     depression_filling: bool = False,
-    expire_after: int = EXPIRE,
-    disable_caching: bool = False,
 ) -> xr.DataArray:
     """Get elevation from DEM data for a grid.
 
@@ -154,10 +151,6 @@ def elevation_bygrid(
     depression_filling : bool, optional
         Fill depressions before sampling using
         `RichDEM <https://richdem.readthedocs.io/en/latest/>`__ package, defaults to False.
-    expire_after : int, optional
-        Expiration time for response caching in seconds, defaults to -1 (never expire).
-    disable_caching : bool, optional
-        If ``True``, disable caching requests, defaults to False.
 
     Returns
     -------
@@ -173,9 +166,7 @@ def elevation_bygrid(
     points = points.to_crs(crs=wms_crs).buffer(2 * resolution)
     bbox = tuple(points.total_bounds)
 
-    dem: xr.DataArray = get_map(  # type: ignore
-        "DEM", bbox, resolution, wms_crs, wms_crs, expire_after, disable_caching
-    )
+    dem: xr.DataArray = get_map("DEM", bbox, resolution, wms_crs, wms_crs)  # type: ignore
 
     attrs = dem.attrs
     attrs["crs"] = pts_crs
@@ -203,17 +194,11 @@ class ElevationByCoords:
     source : str, optional
         Elevation source, defaults to ``tep``. Valid sources are: ``tnm``, ``tep``,
         and ``airmap``.
-    expire_after : int, optional
-        Expiration time for response caching in seconds, defaults to -1 (never expire).
-    disable_caching : bool, optional
-        If ``True``, disable caching requests, defaults to False.
     """
 
     coords: List[Tuple[float, float]]
     crs: Union[str, pyproj.CRS] = DEF_CRS
     source: str = "tep"
-    expire_after: float = EXPIRE
-    disable_caching: bool = False
 
     def __post_init__(self) -> None:
         self.crs = ogc_utils.validate_crs(self.crs)
@@ -239,14 +224,7 @@ class ElevationByCoords:
                 for chunk in coords_chunks
             )
         )
-        elevations = list(
-            tlz.pluck(
-                "data",
-                ar.retrieve_json(
-                    urls, kwds, expire_after=self.expire_after, disable=self.disable_caching
-                ),
-            )
-        )
+        elevations = list(tlz.pluck("data", ar.retrieve_json(urls, kwds)))
         return list(tlz.concat(elevations))
 
     def tnm(self) -> List[float]:
@@ -256,13 +234,7 @@ class ElevationByCoords:
             {"params": {"units": "Meters", "output": "json", "x": f"{x:.5f}", "y": f"{y:.5f}"}}
             for x, y in zip(pts.x, pts.y)
         ]
-        resp = ar.retrieve_json(
-            [ServiceURL().restful.nm_pqs] * len(kwds),
-            kwds,
-            max_workers=5,
-            expire_after=self.expire_after,
-            disable=self.disable_caching,
-        )
+        resp = ar.retrieve_json([ServiceURL().restful.nm_pqs] * len(kwds), kwds, max_workers=5)
         return [
             r["USGS_Elevation_Point_Query_Service"]["Elevation_Query"]["Elevation"] for r in resp
         ]
@@ -274,15 +246,15 @@ class ElevationByCoords:
             layers="3DEPElevation:None",
             outformat="image/tiff",
             crs="EPSG:3857",
-            expire_after=self.expire_after,
-            disable_caching=self.disable_caching,
             validation=False,
         )
         get_dem = tlz.partial(wms_3dep.getmap_bybox, resolution=10, box_crs=wms_3dep.crs)
 
         points_proj = self.coords.to_crs(wms_3dep.crs)  # type: ignore
         bounds = points_proj.buffer(30, cap_style=3)
-        da_list = [geoutils.gtiff2xarray(get_dem(b.bounds)) for b in bounds]
+        da_list: List[xr.DataArray] = [
+            geoutils.gtiff2xarray(get_dem(b.bounds)) for b in bounds  # type: ignore
+        ]
 
         def get_value(da: xr.DataArray, x: float, y: float) -> float:
             nodata = da.attrs["nodatavals"][0]
@@ -293,11 +265,7 @@ class ElevationByCoords:
 
 
 def elevation_bycoords(
-    coords: List[Tuple[float, float]],
-    crs: Union[str, pyproj.CRS] = DEF_CRS,
-    source: str = "tep",
-    expire_after: float = EXPIRE,
-    disable_caching: bool = False,
+    coords: List[Tuple[float, float]], crs: Union[str, pyproj.CRS] = DEF_CRS, source: str = "tep"
 ) -> List[float]:
     """Get elevation for a list of coordinates.
 
@@ -316,10 +284,6 @@ def elevation_bycoords(
         They both tend to be slower than the Airmap service. Note that ``tnm`` is bit unstable.
         It's recommended to use ``tep`` unless 10-m resolution accuracy is not necessary which
         in that case ``airmap`` is more appropriate.
-    expire_after : int, optional
-        Expiration time for response caching in seconds, defaults to -1 (never expire).
-    disable_caching : bool, optional
-        If ``True``, disable caching requests, defaults to False.
 
     Returns
     -------
@@ -327,14 +291,90 @@ def elevation_bycoords(
         Elevation in meter.
     """
     _crs = crs.to_string() if isinstance(crs, pyproj.CRS) else crs
-    service = ElevationByCoords(
-        crs=_crs,
-        coords=coords,
-        source=source,
-        expire_after=expire_after,
-        disable_caching=disable_caching,
-    )
+    service = ElevationByCoords(crs=_crs, coords=coords, source=source)
     return service.__getattribute__(source)()  # type: ignore
+
+
+def __get_spline(line: LineString, ns_pts: int, crs: Union[str, pyproj.CRS]) -> Spline:
+    """Get a B-spline from a line."""
+    x, y = line.xy
+    pts = gpd.GeoSeries(gpd.points_from_xy(x, y, crs=crs))
+    return GeoBSpline(pts, ns_pts).spline
+
+
+def __get_idx(d_sp: np.ndarray, distance: float) -> np.ndarray:  # type: ignore
+    """Get the index of the closest point to a given distance."""
+    dis = pd.DataFrame(d_sp, columns=["distance"]).reset_index()
+    grouper = pd.cut(dis.distance, np.arange(0, dis.distance.max() + distance, distance))
+    return dis.groupby(grouper).last()["index"].to_numpy()  # type: ignore
+
+
+def __get_spline_params(
+    line: LineString, n_seg: int, distance: float, crs: Union[str, pyproj.CRS]
+) -> Tuple[np.ndarray, np.ndarray]:  # type: ignore[type-arg]
+    """Get perpendiculars to a line."""
+    _n_seg = n_seg
+    spline = __get_spline(line, _n_seg, crs)
+    idx = __get_idx(spline.distance, distance)
+    while np.isnan(idx).any():
+        _n_seg *= 2
+        spline = __get_spline(line, _n_seg, crs)
+        idx = __get_idx(spline.distance, distance)
+    return spline.x[idx].flatten(), spline.y[idx].flatten()
+
+
+def elevation_profile(
+    lines: Union[LineString, MultiLineString],
+    spacing: float,
+    dem_res: float = 10,
+    crs: Union[str, pyproj.CRS] = DEF_CRS,
+) -> xr.DataArray:
+    """Get the elevation profile along a line at a given uniform spacing.
+
+    This function converts the line to a B-spline and then calculates the elevation
+    along the spline at a given uniform spacing.
+
+    Parameters
+    ----------
+    lines : LineString or MultiLineString
+        Line segment(s) to be profiled. If ``MultiLineString``,
+        it will be converted to a single ``LineString`` and if this operation
+        fails, a ``InvalidInputType`` will be raised.
+    spacing : float
+        Spacing between the sample points in meters.
+    dem_res : float, optional
+        Resolution of the DEM source to use in meter, defaults to 10.
+    crs : str or pyproj.CRS, optional
+        Spatial reference (CRS) of ``lines``, defaults to ``EPSG:4326``.
+
+    Returns
+    -------
+    xarray.DataArray
+        Elevation profile with dimension ``z``.
+    """
+    if not isinstance(lines, (LineString, MultiLineString)):
+        raise InvalidInputType("lines", "LineString or MultiLineString")
+
+    if isinstance(lines, MultiLineString):
+        path = ops.linemerge(lines)
+        if not isinstance(path, LineString):
+            raise InvalidInputType("lines", "mergeable to a single line")
+    else:
+        path = lines
+
+    crs_prj = "epsg:5070"
+    geom = gpd.GeoSeries([path], crs=crs).to_crs(crs_prj)
+    geom_buff = geom.buffer(10 * dem_res).unary_union
+    dem = get_map("DEM", geom_buff, dem_res, crs_prj)
+
+    n_seg = int(np.ceil(geom.length.sum() / spacing)) * 100
+    x, y = __get_spline_params(geom.geometry[0], n_seg, spacing, crs_prj)
+    xp, yp = zip(*ogc_utils.match_crs(list(zip(x, y)), crs_prj, dem.rio.crs))
+
+    elevation = dem.interp(x=("z", list(xp)), y=("z", list(yp)), method="nearest")
+    xp, yp = zip(*ogc_utils.match_crs(list(zip(x, y)), crs_prj, crs))
+    elevation["x"], elevation["y"] = ("z", list(xp)), ("z", list(yp))
+    return elevation.drop_vars("spatial_ref")  # type: ignore
 
 
 def check_3dep_availability(
@@ -343,7 +383,7 @@ def check_3dep_availability(
     """Query 3DEP's resolution availability within a bounding box.
 
     This function checks availability of 3DEP's at the following resolutions:
-    1 m, 3 m, 5 m, 10 m, 30 m, and 60 m.
+    1 m, 3 m, 5 m, 10 m, 30 m, 60 m, and topobathy (integrated topobathymetry).
 
     Parameters
     ----------
@@ -363,7 +403,7 @@ def check_3dep_availability(
     >>> import py3dep
     >>> bbox = (-69.77, 45.07, -69.31, 45.45)
     >>> py3dep.check_3dep_availability(bbox)
-    {'1m': True, '3m': False, '5m': False, '10m': True, '30m': True, '60m': False}
+    {'1m': True, '3m': False, '5m': False, '10m': True, '30m': True, '60m': False, 'topobathy': False}
     """
     if not isinstance(bbox, Sequence) or len(bbox) != 4:
         raise InvalidInputType("bbox", "a tuple of length 4")
@@ -375,6 +415,7 @@ def check_3dep_availability(
         "10m": 21,
         "30m": 22,
         "60m": 23,
+        "topobathy": 30,
     }
     _bbox = ogc_utils.match_crs(bbox, crs, DEF_CRS)
     url = ServiceURL().restful.nm_3dep_index
@@ -386,3 +427,70 @@ def check_3dep_availability(
         return False
 
     return {res: _check(lyr) for res, lyr in res_layers.items()}
+
+
+def query_3dep_sources(
+    bbox: Tuple[float, float, float, float],
+    crs: Union[str, pyproj.CRS] = DEF_CRS,
+    res: Optional[str] = None,
+) -> gpd.GeoDataFrame:
+    """Query 3DEP's data sources within a bounding box.
+
+    This function queries the availability of the underlying data that 3DEP uses
+    at the following resolutions:
+    1 m, 3 m, 5 m, 10 m, 30 m, 60 m, and topobathy (integrated topobathymetry).
+
+    Parameters
+    ----------
+    bbox : tuple
+        Bounding box as tuple of ``(min_x, min_y, max_x, max_y)``.
+    crs : str or pyproj.CRS, optional
+        Spatial reference (CRS) of bbox, defaults to ``EPSG:4326``.
+    res : str, optional
+        Resolution to query, defaults to ``None``, i.e., all resolutions.
+
+    Returns
+    -------
+    geopandas.GeoDataFrame
+        Polygon(s) representing the 3DEP data sources at each resolution.
+        Resolutions are given in the ``dem_res`` column.
+
+    Examples
+    --------
+    >>> import py3dep
+    >>> bbox = (-69.77, 45.07, -69.31, 45.45)
+    >>> src = py3dep.query_3dep_sources(bbox)
+    >>> src.groupby("dem_res")["OBJECTID"].count().to_dict()
+    {'10m': 8, '1m': 4, '30m': 8}
+    >>> src = py3dep.query_3dep_sources(bbox, res="1m")
+    >>> src.groupby("dem_res")["OBJECTID"].count().to_dict()
+    {'1m': 4}
+    """
+    if not isinstance(bbox, Sequence) or len(bbox) != 4:
+        raise InvalidInputType("bbox", "a tuple of length 4")
+    res_layers = {
+        "1m": 18,
+        "3m": 19,
+        "5m": 20,
+        "10m": 21,
+        "30m": 22,
+        "60m": 23,
+        "topobathy": 30,
+    }
+    if res is not None and res not in res_layers:
+        raise InvalidInputValue("res", list(res_layers.keys()))
+
+    layers = {res: res_layers[res]} if res is not None else res_layers
+
+    _bbox = ogc_utils.match_crs(bbox, crs, DEF_CRS)
+    url = ServiceURL().restful.nm_3dep_index
+
+    def _check(lyr: int) -> Optional[gpd.GeoDataFrame]:
+        wms = utils.RESTful(url, lyr)
+        with contextlib.suppress(ServiceError, TypeError):
+            return wms.bygeom(_bbox)
+        return None
+
+    src = pd.concat({res: _check(lyr) for res, lyr in layers.items()})
+    src = gpd.GeoDataFrame(src.reset_index(level=1, drop=True), crs=DEF_CRS)
+    return src.reset_index().rename(columns={"index": "dem_res"})
