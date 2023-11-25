915bf32473216a29ca0641249a1d779e7692bd05
Taher Chegini <cheginit@gmail.com>
2023-11-25 18:31:32 -0500

    MNT: Remove the unused functions. [skip ci]

--- py3dep/utils.py
+++ py3dep/utils.py
@@ -6,13 +6,8 @@ from typing import TYPE_CHECKING, Literal, Sequence, Union, overload
 import numpy as np
 import xarray as xr
 
-import pygeoutils as geoutils
-from pygeoogc import ArcGISRESTful
-
 if TYPE_CHECKING:
-    import geopandas as gpd
     import pyproj
-    from shapely import Polygon
 
     CRSTYPE = Union[int, str, pyproj.CRS]
 
@@ -118,85 +113,3 @@ def rename_layers(
     else:
         ds = ds.rename({n: rename[str(n)] for n in ds})
     return ds
-
-
-class RESTful:
-    """Base class for getting geospatial data from a ArcGISRESTful service.
-
-    Parameters
-    ----------
-    base_url : str, optional
-        The ArcGIS RESTful service url. The URL must either include a layer number
-        after the last ``/`` in the url or the target layer must be passed as an argument.
-    layer : int, optional
-        A valid service layer.
-    outfields : str or list, optional
-        Target field name(s), default to "*" i.e., all the fields.
-    crs : str, int, or pyproj.CRS, optional
-        Target spatial reference, default to EPSG:4326
-    outformat : str, optional
-        One of the output formats offered by the selected layer. If not correct
-        a list of available formats is shown, defaults to ``json``.
-    """
-
-    def __init__(
-        self,
-        base_url: str,
-        layer: int,
-        outfields: str | list[str] = "*",
-        crs: CRSTYPE = 4326,
-        outformat: str = "json",
-    ) -> None:
-        self.client = ArcGISRESTful(
-            base_url,
-            layer,
-            outformat=outformat,
-            outfields=outfields,
-            crs=crs,
-        )
-
-    def bygeom(
-        self,
-        geom: Polygon | list[tuple[float, float]] | tuple[float, float, float, float],
-        geo_crs: CRSTYPE = 4326,
-    ) -> gpd.GeoDataFrame:
-        """Get feature within a geometry that can be combined with a SQL where clause.
-
-        Parameters
-        ----------
-        geom : Polygon or tuple
-            A geometry (Polygon) or bounding box (tuple of length 4).
-        geo_crs : str, int, or pyproj.CRS
-            The spatial reference of the input geometry.
-
-        Returns
-        -------
-        geopandas.GeoDataFrame
-            The requested features as a GeoDataFrame.
-        """
-        oids = self.client.oids_bygeom(geom, geo_crs=geo_crs)
-        return geoutils.json2geodf(self.client.get_features(oids), self.client.client.crs)
-
-    def bysql(
-        self,
-        sql_clause: str,
-    ) -> gpd.GeoDataFrame:
-        """Get feature IDs using a valid SQL 92 WHERE clause.
-
-        Notes
-        -----
-        Not all web services support this type of query. For more details look
-        `here <https://developers.arcgis.com/rest/services-reference/query-feature-service-.htm#ESRI_SECTION2_07DD2C5127674F6A814CE6C07D39AD46>`__
-
-        Parameters
-        ----------
-        sql_clause : str
-            A valid SQL 92 WHERE clause.
-
-        Returns
-        -------
-        geopandas.GeoDataFrame
-            The requested features as a GeoDataFrame.
-        """
-        oids = self.client.oids_bysql(sql_clause)
-        return geoutils.json2geodf(self.client.get_features(oids), self.client.client.crs)
